f1e5e64e2c4271a3548081c744c5f79ec4970fa5
OdysseyDev <93072230+StickOdyssey@users.noreply.github.com>
2024-06-29 04:09:34 -0400

    Idk

--- TestProject/TestProject/Form1.cs
+++ TestProject/TestProject/Form1.cs
@@ -1,8 +1,5 @@
 using System.Diagnostics;
-using System.Net;
 using System.Net.Http.Headers;
-using System.Windows.Forms;
-using HtmlAgilityPack;
 
 namespace TestProject
 {
--- TestProject/TestProject/obj/Debug/net8.0-windows/TestProject.AssemblyInfo.cs
+++ TestProject/TestProject/obj/Debug/net8.0-windows/TestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2626a70c48f876141991296350898af1cb2d4742")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
